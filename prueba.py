1b5c3baee5b79e6f80a82512ab38c9cbe1224ab0
Perla Itzel Salazar <perlaitzelsalazartrejo@gmail.com>
2025-09-25 17:56:48 -0600

    programa de nombre

---
+++ prueba.py
@@ -0,0 +1,5 @@
+# 1. Pedir al usuario que ingrese su nombre y guardarlo en una variable.
+nombre = input("¿Cómo te llamas? ")
+
+# 2. Imprimir un saludo personalizado usando el nombre ingresado.
+print(f"¡Hola, {nombre}! Bienvenido a Python.")
